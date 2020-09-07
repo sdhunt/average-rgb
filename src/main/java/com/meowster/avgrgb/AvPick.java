4e806947c69aed19479dab03a167ff5a74d814a7
Simon Hunt <sdhunt@gmail.com>
2020-09-07 16:15:51 -0700

    Added Main Class.

---
+++ src/main/java/com/meowster/avgrgb/AvPick.java
@@ -0,0 +1,10 @@
+package com.meowster.avgrgb;
+
+public class AvPick {
+    public static final String VERSION = "1.0a";
+
+    public static void main(String[] args) {
+        System.out.println("AvPick: version " + VERSION);
+        System.out.println("Let's do this!");
+    }
+}
