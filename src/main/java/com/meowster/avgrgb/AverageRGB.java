65642b20f2cc688578ec0d7ead132eb838beeff7
Simon Hunt <sdhunt@gmail.com>
2020-09-19 14:20:47 -0700

    Added ant build to generate distribution JAR and README.

--- src/main/java/com/meowster/avgrgb/AverageRGB.java
+++ src/main/java/com/meowster/avgrgb/AverageRGB.java
@@ -10,7 +10,7 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class AverageRGB {
-    public static final String VERSION = "1.0b";
+    public static final String VERSION = "1.1";
 
     public static void main(String[] args) {
         System.out.println(version());
